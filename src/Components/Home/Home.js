2f4f808b6a164a2c72fb4a06887525ac6433eee6
mdegroot09 <mdegroot09@gmail.com>
2021-12-07 16:17:34 -0700

    Check for winner on each square update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -15,12 +15,60 @@ class Home extends Component {
         let e = document.getElementById(id)
         e.innerText = mark
         e.style.pointerEvents = 'none'
-        let isGameOver = this.checkForWinner(id)
+        let isGameOver = this.checkForWinner(mark, id)
         isGameOver ? this.completeGame() : this.setState({turnX: !turnX})
     }
 
-    checkForWinner = (id) => {
-        return false
+    checkForWinner = (mark, id) => {
+        let a1 = document.getElementById('a1').innerText
+        let a2 = document.getElementById('a2').innerText
+        let a3 = document.getElementById('a3').innerText
+        let b1 = document.getElementById('b1').innerText
+        let b2 = document.getElementById('b2').innerText
+        let b3 = document.getElementById('b3').innerText
+        let c1 = document.getElementById('c1').innerText
+        let c2 = document.getElementById('c2').innerText
+        let c3 = document.getElementById('c3').innerText
+        switch (id){
+            case 'a1':
+                if ((a1 === mark && a2 === mark && a3 === mark) || (a1 === mark && b1 === mark && c1 === mark) || (a1 === mark && b2 === mark && c3 === mark)){
+                    return true
+                } else {return false}
+            case 'a2':
+                if ((a1 === mark && a2 === mark && a3 === mark) || (a2 === mark && b2 === mark && c2 === mark)){
+                    return true
+                } else {return false}
+            case 'a3':
+                if ((a1 === mark && a2 === mark && a3 === mark) || (a3 === mark && b3 === mark && c3 === mark) || (a3 === mark && b2 === mark && c1 === mark)){
+                    return true
+                } else {return false}
+            case 'b1':
+                if ((b1 === mark && b2 === mark && b3 === mark) || (a1 === mark && b1 === mark && c1 === mark)){
+                    return true
+                } else {return false}
+            case 'b2':
+                if ((b1 === mark && b2 === mark && b3 === mark) || (a2 === mark && b2 === mark && c2 === mark) || (a1 === mark && b2 === mark && c3 === mark) || (a3 === mark && b2 === mark && c1 === mark)){
+                    return true
+                } else {return false}
+            case 'b3':
+                if ((b1 === mark && b2 === mark && b3 === mark) || (a3 === mark && b3 === mark && c3 === mark)){
+                    return true
+                } else {return false}
+            case 'c1':
+                if ((c1 === mark && c2 === mark && c3 === mark) || (a3 === mark && b3 === mark && c3 === mark)){
+                    return true
+                } else {return false}
+            case 'c2':
+                if ((c1 === mark && c2 === mark && c3 === mark) || (a2 === mark && b2 === mark && c2 === mark)){
+                    return true
+                } else {return false}
+            case 'c3':
+                if ((c1 === mark && c2 === mark && c3 === mark) || (a3 === mark && b3 === mark && c3 === mark) || (a1 === mark && b2 === mark && c3 === mark)){
+                    return true
+                } else {return false}
+            default:
+                return false
+        }
     }
 
     completeGame = () => {
@@ -42,6 +90,7 @@ class Home extends Component {
         return(
             <div className="home">
                 <h1 id="winner" style={{display: 'none'}}>{turnX} WINS!</h1>
+                <h1 id="catsGame" style={{display: 'none'}}>Cat's Game!</h1>
                 <h1 id="turn">{turnX}'s turn</h1>
                 <div className="board">
                     <div className="column">
